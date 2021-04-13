89b38597918d172d8e2502f703868dda27092e98
magali <digimaglink@gmail.com>
2021-04-13 18:05:18 +0200

    ajout outils front #2

---
+++ groupomania/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-primary">
+    <h1> Test </h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
