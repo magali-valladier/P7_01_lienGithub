c0f54ebdd3d65ffdd8995fd7d908c8533fb06aba
magali <digimaglink@gmail.com>
2021-04-18 17:36:01 +0200

    page post component #13

--- groupomania/src/router/index.js
+++ groupomania/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Post from '../views/Post.vue'
+import Signin from '../views/Signin.vue'
 
 
 Vue.use(VueRouter)
@@ -12,24 +14,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/post',
     name: 'Post',
-    
-    component: () => import(/* webpackChunkName: "Post" */ '../views/Post.vue')
+    component: Post
   },
   {
     path: '/signin',
     name: 'Signin',
-    
-    component: () => import(/* webpackChunkName: "Post" */ '../views/Post.vue')
+    component: Signin
   },
 ]
 
--- groupomania/src/views/Post.vue
+++ groupomania/src/views/Post.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="post">
    
   <Navbar />
   <Footer />
