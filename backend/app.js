a11f956a7eb172ed20830b35b202a2aa767d5dc7
magali <digimaglink@gmail.com>
2021-04-18 11:38:10 +0200

    mise en place back #9

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const path = require("path");
+const xss = require('xss');
+const helmet = require('helmet');
+const rateLimit = require("express-rate-limit");
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+
+  //input sanitization against XXS attacks(helmet also does the same in this package)
+app.use(xss());
+
+//Set HTTP headers with helmet
+app.use(helmet());
+
+//limit several sessions in a shortime to avoid force's attacks
+app.use(rateLimit());
+
+module.exports = app;
