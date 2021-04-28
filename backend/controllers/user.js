f9c4b3603ad0b0d9420cf633d3bdbdccdbdb5452
magali <digimaglink@gmail.com>
2021-04-28 12:58:50 +0200

    connexion post

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -21,37 +21,35 @@ exports.signup = async (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
       
     }
-exports.login = (req, res, next) => {
+    exports.login = (req, res, next) => {  
 
-  const User = db.user;
-
-  User.findOne({ 
-    where: { 
-      
-      email:req.body.email,
-      
+      if ( !req.body.email && !req.body.password ) {
+        return res.status(400).json({message: "champ manquant"})
     }
-  }) 
-    .then(user => {
-      if (!user) { 
-        return res.status(404).json({ error: 'Utilisateur non trouvé !' });
-      }
-      bcrypt.compare(req.body.password, user.password) 
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+
+      User.findOne({
+        where: {
+          email: req.body.email,
+        }
+      })
+        .then((user) => {
+          if (!user) {
+            return res.status(401).json({ error: 'Utilisateur inconnu' });
           }
-          console.log(req.body.password);
-          res.status(200).json({ 
-            userId: user._id,
-            token: jwt.sign(
-              { userId: user._id },
-              process.env.SECRET_TOKEN,
-              { expiresIn: '24h' }
-              )
-          })
+          bcrypt.compare(req.body.password, user.password) 
+            .then((valid) => {
+              if (!valid) {
+                return res.status(401).json({ error: 'Utilisateur ou mot de passe erroné' });
+              }
+              res.status(200).json({
+                token: jwt.sign(
+                  { id: user.id }, 'RANDOM_TOKEN_SECRET',
+                  { expiresIn: '24h' }),
+                });
+              
+            });
         })
-        .catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
-};
+        .catch((err) => {
+          res.status(500).json({ error: err });
+        });
+    };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,6 +11,6 @@ const multer = require('../middleware/multer-config');
 // Routes
 
 router.post('/signup', multer, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login',auth, userCtrl.login);
 
 module.exports = router;
